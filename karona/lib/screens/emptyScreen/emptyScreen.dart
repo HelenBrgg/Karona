620d91921c5e79b13356601a8810486cd351763b
Helen Brüggmann <helenbruggmann@Helens-MacBook-Pro.local>
2020-03-28 14:49:58 +0100

    startForEmptyScreen

---
+++ karona/lib/screens/emptyScreen/emptyScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class EmptyScreen extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('Hello')
+      ),
+      body:Text(''),
+    );
+  }
+}
