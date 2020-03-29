707825390b538389e9924af4387df46df1a867b9
undty <undty@student.kit.edu>
2020-03-29 16:36:32 +0200

    Encapsulated Code for sql interfaces in classes. Implemented an SQL Interface for challenges and one for networks.

--- karona/lib/persistency/network_sql_interface.dart
+++ karona/lib/persistency/network_sql_interface.dart
@@ -5,22 +5,22 @@ import 'package:sqflite/sqflite.dart';
 import 'package:flutter/widgets.dart';
 
 
-import './challenge_classes.dart';
+import './network_classes.dart';
 
-class Challenge_SQL_Interface
+class Networks_SQL_Interface
 {
   Future<Database> database;
   void Init_SQL_Interface() async
   {
-    this.database = openDatabase(
+    database = openDatabase(
     // Set the path to the database. Note: Using the `join` function from the
     // `path` package is best practice to ensure the path is correctly
     // constructed for each platform.
-    join(await getDatabasesPath(), 'challenge_database.db'),
+    join(await getDatabasesPath(), 'networks_database.db'),
     // When the database is first created, create a table to store dogs.
     onCreate: (db, version) {
       return db.execute(
-        "CREATE TABLE challenges(id INTEGER PRIMARY KEY, challengeText TEXT)",
+        "CREATE TABLE networks(id INTEGER PRIMARY KEY, ssid TEXT, name TEXT)",
       );
     },
     // Set the version. This executes the onCreate function and provides a
@@ -30,60 +30,59 @@ class Challenge_SQL_Interface
   return;
   }
 
-  Future<void> insertChallenge(Challenge challenge) async {
+  Future<void> insertNetwork(Network network) async {
     // Get a reference to the database.
     final Database db = await database;
 
-    print("##################     Got Database");
     // Insert the Dog into the correct table. Also specify the
     // `conflictAlgorithm`. In this case, if the same dog is inserted
     // multiple times, it replaces the previous data.
     await db.insert(
-      'challenges',
-      challenge.toMap(),
+      'networks',
+      network.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
-    print("#################    Awaited insert");
   }
 
-   Future<List<Challenge>> challenges() async {
+   Future<List<Network>> networks() async {
     // Get a reference to the database.
     final Database db = await database;
 
     // Query the table for all The Dogs.
-    final List<Map<String, dynamic>> maps = await db.query('challenges');
+    final List<Map<String, dynamic>> maps = await db.query('networks');
 
     // Convert the List<Map<String, dynamic> into a List<Dog>.
     return List.generate(maps.length, (i) {
-      return Challenge(
+      return Network(
         id: maps[i]['id'],
-        challengeText: maps[i]['challengeText'],
+        ssid: maps[i]['ssid'],
+        name: maps[i]['name'],
       );
     });
   }
 
-  Future<void> updateChallenge(Challenge challenge) async {
+  Future<void> updateNetwork(Network network) async {
     // Get a reference to the database.
     final db = await database;
 
     // Update the given Dog.
     await db.update(
-      'challenges',
-      challenge.toMap(),
+      'networks',
+      network.toMap(),
       // Ensure that the Dog has a matching id.
       where: "id = ?",
       // Pass the Dog's id as a whereArg to prevent SQL injection.
-      whereArgs: [challenge.id],
+      whereArgs: [network.id],
     );
   }
 
-   Future<void> deleteChallenge(int id) async {
+   Future<void> deleteNetwork(int id) async {
     // Get a reference to the database.
     final db = await database;
 
     // Remove the Dog from the database.
     await db.delete(
-      'challenges',
+      'networks',
       // Use a `where` clause to delete a specific dog.
       where: "id = ?",
       // Pass the Dog's id as a whereArg to prevent SQL injection.
@@ -91,64 +90,91 @@ class Challenge_SQL_Interface
     );
   }
 
-  Future<Challenge> getChallengeById(int id) async {
+  Future<Network> getNetworkById(int id) async {
   final db = await database;
-  List<Map> results = await db.query('challenges',
+  List<Map> results = await db.query('networks',
       where: 'id = ?',
       whereArgs: [id]);
 
   if (results.length > 0) {
-    return new Challenge.fromMap(results.first);
+    return new Network.fromMap(results.first);
+  }
   }
 
+  Future<Network> getNetworkBySSID(String ssid) async {
+  final db = await database;
+  List<Map> results = await db.query('networks',
+      where: 'ssid = ?',
+      whereArgs: [ssid]);
+
+  if (results.length > 0) {
+    return new Network.fromMap(results.first);
+  }
   return null;
 }
 
 
 }
 
+
+
+// exemplary code
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  print(" #########       Launching Main");
 
-  Challenge_SQL_Interface db_interface = new Challenge_SQL_Interface();
-  await db_interface.Init_SQL_Interface();
+  Networks_SQL_Interface networks_sql_interface = new Networks_SQL_Interface();
+  await networks_sql_interface.Init_SQL_Interface();
 
 
-  var clg_call_grandma = Challenge(
+  var network_a = Network(
     id: 0,
-    challengeText: 'Call your Grandma!',
+    ssid: 'XXDA',
+    name: 'homebase'
   );
 
-  print(" #########       Inserting");
-  // Insert a dog into the database.
-  await db_interface.insertChallenge(clg_call_grandma);
+   var network_b = Network(
+    id: 1,
+    ssid: 'UUPP',
+    name: 'my_neighbours'
+  );
 
-  print(" #########       Printing List");
+  // Insert a dog into the database.
+  await networks_sql_interface.insertNetwork(network_a);
+  await networks_sql_interface.insertNetwork(network_b);
+
+  print("\nListing all Networks in the sql database:");
   // Print the list of dogs (only Fido for now).
-  print(await db_interface.challenges());
+  print(await networks_sql_interface.networks());
 
   // Update Fido's age and save it to the database.
-  clg_call_grandma = Challenge(
-    id: 0,
-    challengeText: 'Call her NOW!',
+ network_b = Network(
+    id: 1,
+    ssid: 'PPID',
+    name: 'my_flat'
   );
-  await db_interface.updateChallenge(clg_call_grandma);
+  await networks_sql_interface.updateNetwork(network_b);
 
   // Print Fido's updated information.
-  print(await db_interface.challenges());
+  print(await networks_sql_interface.networks());
 
   // query the database for fido's id (0)
-  Challenge query = await db_interface.getChallengeById(0) ;
+  Network query = await networks_sql_interface.getNetworkById(0) ;
   
-  print('Here\'s challenge #0:');
+  print('\nHere\'s the Network with ID = 0:');
+  print(query);
+
+  query = await networks_sql_interface.getNetworkBySSID('PPID') ;
+  
+  print('\nHere\'s the Network with SSID = PPID:');
   print(query);
   
+  print("\nDeleting Network:  " + network_b.toString());
   // Delete Fido from the database.
-  await db_interface.deleteChallenge(clg_call_grandma.id);
+  await networks_sql_interface.deleteNetwork(network_b.id);
 
+  print("\nList of Networks Left:");
   // Print the list of dogs (empty).
-  print(await db_interface.challenges());
+  print(await networks_sql_interface.networks());
   
 }
 
