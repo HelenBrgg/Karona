9227f08ce0c7b405fc4fd14af9f75b0a9d6cd2e0
Marius Gerdes <gerdes.marius@gmail.com>
2020-03-30 14:46:02 +0200

    first attempts at wifipage layout

--- karona/lib/wifi/wifiNetworkManager.dart
+++ karona/lib/wifi/wifiNetworkManager.dart
@@ -13,11 +13,34 @@ class WifiNetworkManager extends StatelessWidget {
             style: Theme.of(context).textTheme.title,
           ),
           const SizedBox(height: 8),
-          Text(
-            'Hello There',
-            style: Theme.of(context).textTheme.subtitle,
+          Container(
+            height: 500.0,
+            child: ListView(
+              //scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Card(child: ListTile(title: Text('One-line ListTile'))),
+                Card(
+                  child: ListTile(
+                    leading: FlutterLogo(),
+                    title: Text('One-line with leading widget'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text('One-line with trailing widget'),
+                    trailing: Icon(Icons.more_vert),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: FlutterLogo(),
+                    title: Text('One-line with both widgets'),
+                    trailing: Icon(Icons.more_vert),
+                  ),
+                ),
+              ],
+            ),
           ),
-          const SizedBox(height: 8),
           RaisedButton(
             child: Text('Back'),
             onPressed: () => Navigator.pop(context),
